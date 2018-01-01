2eb5e3f85fb2af487a51a6645f3b5e536a27fd23
Willyam Santana Almeida <willyamalmeida@hotmail.com>
2018-01-01 01:38:50 -0200

    ajustes

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
 
 const WebpackDevServer = require('webpack-dev-server');
-const WebpackDevMiddleware = require('webpack-dev-middleware');
+//const WebpackDevMiddleware = require('webpack-dev-middleware');
 const config = require('./webpack.config');
 const webpack = require('webpack');
-const express = require('express');
+// const express = require('express');
 
-const app = express();
+// const app = express();
 const compiler = webpack(config);
 
-if (process.env.NODE_ENV === 'production') {
-    let appInsights = require('applicationinsights');
+// if (process.env.NODE_ENV === 'production') {
+//     let appInsights = require('applicationinsights');
 
-    appInsights.setup(process.env.APPINSIGHTS_INSTRUMENTATIONKEY).start();
-    let aiClient = appInsights.defaultClient;
+//     appInsights.setup(process.env.APPINSIGHTS_INSTRUMENTATIONKEY).start();
+//     let aiClient = appInsights.defaultClient;
 
-    app.use(function (req, res, next) {
-        aiClient.trackRequest(req, res);
-        next();
-    });
+//     app.use(function (req, res, next) {
+//         aiClient.trackRequest(req, res);
+//         next();
+//     });
 
-    app.use(function (err, req, res, next) {
-        aiClient.trackException(err);
-    });
-}
+//     app.use(function (err, req, res, next) {
+//         aiClient.trackException(err);
+//     });
+// }
 
-const webpackMiddleware = WebpackDevMiddleware(compiler, config.devServer);
-app.listen(8080, webpackMiddleware.listen);
+// const webpackMiddleware = WebpackDevMiddleware(compiler, config.devServer);
+// app.listen(8080, webpackMiddleware.listen);
 
-//new WebpackDevServer(webpack(config), config.devServer).listen(process.env.PORT || 8080);
+new WebpackDevServer(webpack(config), config.devServer).listen(process.env.PORT || 8080);
